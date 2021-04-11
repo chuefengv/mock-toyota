51efe5e4abb20c997037db3f4960ee9db3fd4278
chuefengv <feng.vang98@gmail.com>
2021-04-11 19:01:45 -0400

    working external controls on carousel

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -1,16 +1,53 @@
-import React from 'react';
+import React ,{useState, useEffect} from 'react';
 import './Hero.css';
-import Slider from './slider/Slider';
 import {one, two, three, four, five} from '../../assets/hero-image/index';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+
 function Hero(){
+    const [index, setIndex] = useState(0);
+  
+    function next(){
+        setIndex(index+1)
+        if(index==4){
+            setIndex(0)
+        }
+    }
+
+    useEffect(()=>{
+        console.log(index)
+    },[index])
+
     return(
         <div className='hero-wrapper'>
-            <div className='hero-banner'>
-                <img src={one} alt=''></img>
-            </div>
-            <div>
-                INFORMATION HERE
-                <Slider />
+            <div className='hero-slider'>
+                <div>
+                    <button onClick={()=>next()}>Next Button</button>
+                </div>
+                <Carousel selectedItem={index} showArrows={false} showThumbs={false} showStatus={false} infiniteLoop={true}>
+                    <div>
+                        <img src={one} alt=''></img>
+                        <h2>Text</h2>
+                    </div>
+                    <div>
+                        <img src={two} alt=''></img>
+                        <h2>Text</h2>
+                    </div>
+                    <div>
+                        <img src={three} alt=''></img>
+                        <h2>Text</h2>
+                    </div>
+                    <div>
+                        <img src={four} alt=''></img>
+                        <h2>Text</h2>
+                    </div>
+                    <div>
+                        <img src={five} alt=''></img>
+                        <h2>Text</h2>
+                    </div>
+                </Carousel>
+
             </div>
         </div>
     )
