be69732aebd5d333651eec813271e1081de69ede
chuefengv <feng.vang98@gmail.com>
2021-04-11 22:40:50 -0400

    bubbles are working finally

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -1,8 +1,10 @@
-import React ,{useState, useEffect} from 'react';
+import React ,{useState} from 'react';
 import './Hero.css';
 import {one, two, three, four, five} from '../../assets/hero-image/index';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 
 function Hero(){
@@ -10,45 +12,125 @@ function Hero(){
   
     function next(){
         setIndex(index+1)
-        if(index==4){
+        if(index===4){
             setIndex(0)
         }
     }
 
-    useEffect(()=>{
-        console.log(index)
-    },[index])
+    function prev(){
+        setIndex(index-1)
+        if(index===0){
+            setIndex(4)
+        }
+    }
 
     return(
         <div className='hero-wrapper'>
+
             <div className='hero-slider'>
-                <div>
-                    <button onClick={()=>next()}>Next Button</button>
-                </div>
-                <Carousel selectedItem={index} showArrows={false} showThumbs={false} showStatus={false} infiniteLoop={true}>
+                <Carousel selectedItem={index} showArrows={false} showIndicators={false} showThumbs={false} showStatus={false} infiniteLoop={true}>
                     <div>
-                        <img src={one} alt=''></img>
-                        <h2>Text</h2>
+                        <div>
+                            <img src={one} alt=''></img>
+                        </div>
+                        <div className='hero-slider-description'>
+                            <p>Options shown</p>
+                            <p>WITH THE <strong>2021 PRIUS</strong>, GET</p>
+                            <p>$1,500</p>
+                            <p>Cash Back</p>
+                            <button>View Offer</button>
+                            <button>Explore</button>
+                        </div>
                     </div>
                     <div>
-                        <img src={two} alt=''></img>
-                        <h2>Text</h2>
+                        <div>
+                            <img src={two} alt=''></img>
+                        </div>
+                        <div className='hero-slider-description'>
+                            <p>Options shown</p>
+                            <p>WITH THE <strong>2021 PRIUS</strong>, GET</p>
+                            <p>$1,500</p>
+                            <p>Cash Back</p>
+                            <button>View Offer</button>
+                            <button>Explore</button>
+                        </div>
                     </div>
                     <div>
-                        <img src={three} alt=''></img>
-                        <h2>Text</h2>
+                        <div>
+                            <img src={three} alt=''></img>
+                        </div>
+                        <div className='hero-slider-description'>
+                            <p>Options shown</p>
+                            <p>WITH THE <strong>2021 PRIUS</strong>, GET</p>
+                            <p>$1,500</p>
+                            <p>Cash Back</p>
+                            <button>View Offer</button>
+                            <button>Explore</button>
+                        </div>
                     </div>
                     <div>
-                        <img src={four} alt=''></img>
-                        <h2>Text</h2>
+                        <div>
+                            <img src={four} alt=''></img>
+                        </div>
+                        <div className='hero-slider-description'>
+                            <p>Options shown</p>
+                            <p>WITH THE <strong>2021 PRIUS</strong>, GET</p>
+                            <p>$1,500</p>
+                            <p>Cash Back</p>
+                            <button>View Offer</button>
+                            <button>Explore</button>
+                        </div>
                     </div>
                     <div>
-                        <img src={five} alt=''></img>
-                        <h2>Text</h2>
+                        <div>
+                            <img src={five} alt=''></img>
+                        </div>
+                        <div className='hero-slider-description'>
+                            <p>Options shown</p>
+                            <p>WITH THE <strong>2021 PRIUS</strong>, GET</p>
+                            <p>$1,500</p>
+                            <p>Cash Back</p>
+                            <button>View Offer</button>
+                            <button>Explore</button>
+                        </div>
                     </div>
                 </Carousel>
-
             </div>
+            <div className='hero-arrows-left' onClick={()=>prev()}>
+                    <FontAwesomeIcon icon={faChevronLeft} ></FontAwesomeIcon>
+            </div>
+
+
+            <div className='hero-arrows-right' onClick={()=>next()}>
+                    <FontAwesomeIcon icon={faChevronRight} ></FontAwesomeIcon>
+            </div>
+            <div className='header-indicators'>
+                <div className='bubble'>
+                    {index===0 && <div className='header-indicator-selected'></div>}
+                    {index!==0 && <div className='header-indicator-unselected' onClick={()=>setIndex(0)}></div>}
+                </div>
+
+                <div className='bubble'>
+                    {index===1 && <div className='header-indicator-selected'></div>}
+                    {index!==1 && <div className='header-indicator-unselected' onClick={()=>setIndex(1)}></div>}
+                </div>
+                
+                <div className='bubble'>
+                    {index===2 && <div className='header-indicator-selected'></div>}
+                    {index!==2 && <div className='header-indicator-unselected' onClick={()=>setIndex(2)}></div>}
+                </div>
+
+                <div className='bubble'>
+                    {index===3 && <div className='header-indicator-selected'></div>}
+                    {index!==3 && <div className='header-indicator-unselected' onClick={()=>setIndex(3)}></div>}
+                </div>
+
+                <div className='bubble'>
+                    {index===4 && <div className='header-indicator-selected'></div>}
+                    {index!==4 && <div className='header-indicator-unselected' onClick={()=>setIndex(4)}></div>}
+                </div>
+            </div>
+
         </div>
     )
 }
