b522ef6740dfdf7b21af3321d129d104007048c4
chuefengv <feng.vang98@gmail.com>
2021-04-15 00:25:43 -0400

    desktop header set, need to work media queries transitioning

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -3,12 +3,14 @@ import './Header.css';
 import toyota from '../../assets/toyota.svg';
 import olympic from '../../assets/olympic.svg'
 import paralympic from '../../assets/paralympic.svg'
+import toyota_big from '../../assets/toyota_big.svg'
 import location from '../../assets/location.svg';
 import { Fade as Hamburger } from 'hamburger-react'
 import {Collapse} from '@material-ui/core'
 import MenuOne from './hamburger-menu/MainMenu';
 import LoadedMenu from './hamburger-menu/LoadedMenu';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 
 import { Carousel } from 'react-responsive-carousel';
@@ -44,11 +46,38 @@ function Header({openMenu, setOpenMenu}){
                 </div>
             </div>
 
+            <div className='header-toyota'>
+                <img src={toyota_big} alt=''></img>   
+            </div>
+
             <div className='header-olympics'>
                 <img src={olympic} alt=''></img>
                 <img src={paralympic} alt=''></img>
             </div>
 
+            <div className='header-nav-two'>
+            <div class='header-nav-two-select'>
+                    <div>Select Tools</div>
+                    <FontAwesomeIcon icon={faChevronDown} color={'rgb(204, 0, 0)'} ></FontAwesomeIcon>
+
+                </div>
+                <div class='header-nav-two-select'>
+                    <div>Shopping Tools</div>
+                    <FontAwesomeIcon icon={faChevronDown} color={'rgb(204, 0, 0)'} ></FontAwesomeIcon>
+                </div>
+                <div class='header-nav-two-select'>
+                    Find a Dealer
+                </div>
+                <div class='header-nav-two-select'>
+                    Build & Price
+                </div>
+                <div class='header-nav-two-red'>
+                    <div>
+                        Local Specials
+                    </div>
+                </div>
+            </div>
+{/* 
             <div className='header-nav'>
                 <div className='header-nav-location'>
                     <img src={location} alt=''></img>
@@ -57,7 +86,7 @@ function Header({openMenu, setOpenMenu}){
                 <div className='header-nav-ham'>
                     <Hamburger label="Show menu" size={26} className='header-hamburger' onToggle={()=>menuFunc()}/>
                 </div>
-            </div>
+            </div> */}
 
             <Collapse in={openMenu} timeout={400} className='menu'>
                 <Carousel selectedItem={index} showArrows={false} showIndicators={false} showThumbs={false} showStatus={false}>
