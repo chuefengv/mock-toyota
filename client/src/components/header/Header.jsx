eab11cec7868f81a84240bbfd5ef2ffc66ca0423
chuefengv <feng.vang98@gmail.com>
2021-04-15 21:56:56 -0400

    finished secondary desktop nav

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
 import toyota from '../../assets/toyota.svg';
 import olympic from '../../assets/olympic.svg'
@@ -10,7 +10,7 @@ import {Collapse} from '@material-ui/core'
 import MenuOne from './hamburger-menu/MainMenu';
 import LoadedMenu from './hamburger-menu/LoadedMenu';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faSearch, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
 
 import { Carousel } from 'react-responsive-carousel';
@@ -18,7 +18,11 @@ import { Carousel } from 'react-responsive-carousel';
 
 function Header({openMenu, setOpenMenu}){
 
+    
     const [index, setIndex] = useState(0);
+    const [deskNav, setDeskNav] = useState((window.innerWidth>=820) ? true : false);
+    const [mobNav, setMobNav] = useState((window.innerWidth<820) ? true: false);
+    const [toyotaLogo, setToyotaLogo] = useState((window.innerWidth>=820) ? true : false);
     const [menu01, setMenu01] = useState(false);
     const [menu02, setMenu02] = useState(false);
     const [menu03, setMenu03] = useState(false);
@@ -26,59 +30,97 @@ function Header({openMenu, setOpenMenu}){
 
     function menuFunc(){
         setOpenMenu(!openMenu)
-
-
         if(document.body.style.overflow === 'hidden'){
             document.body.style.overflow = 'scroll'
             setIndex(0)
         }else{
             document.body.style.overflow = 'hidden'
-
+            setIndex(0)
         }
         window.scrollTo(0,0);
     }
 
+    useEffect(()=>{
+        function renderNav(){
+            if(window.innerWidth>=820){
+                setDeskNav(true);
+                setMobNav(false);
+                setOpenMenu(false);
+                setToyotaLogo(true)
+            }else{
+                setDeskNav(false);
+                setMobNav(true);
+                setToyotaLogo(false);
+            }
+        }
+
+        window.addEventListener('resize',renderNav)
+    },[setOpenMenu])
+
+
     return(
         <div className='header-wrapper'>
+
+            {deskNav && <div className='header-secondary'>
+                <div className='header-secondary-select'>
+                    <div>Search <FontAwesomeIcon icon={faSearch} color={'rgb(204, 0, 0)'} ></FontAwesomeIcon></div>
+                </div>
+                <div className='header-secondary-select'>
+                    <div>FAQ</div>
+                </div>
+                <div className='header-secondary-select'>
+                    <div>Certified Used Vehicles</div>
+                </div>
+                <div className='header-secondary-select'>
+                    <div>Owners</div>
+                </div>
+                <div className='header-secondary-select'>
+                    <div>Español</div>
+                </div>
+                <div className='header-secondary-select'>
+                    <div>Your Location: 90210 <FontAwesomeIcon icon={faPencilAlt} color={'rgb(204, 0, 0)'} ></FontAwesomeIcon></div>
+                </div>
+            </div>}
+
             <div className='header-logo'>
                 <div className='header-logo-box'>
                     <img src={toyota} alt=''></img>
                 </div>
             </div>
 
-            <div className='header-toyota'>
+            {toyotaLogo && <div className='header-toyota'>
                 <img src={toyota_big} alt=''></img>   
-            </div>
+            </div>}
 
             <div className='header-olympics'>
                 <img src={olympic} alt=''></img>
                 <img src={paralympic} alt=''></img>
             </div>
 
-            <div className='header-nav-two'>
-            <div class='header-nav-two-select'>
+            {deskNav && <div className='header-nav-two'>
+                <div className='header-nav-two-select'>
                     <div>Select Tools</div>
                     <FontAwesomeIcon icon={faChevronDown} color={'rgb(204, 0, 0)'} ></FontAwesomeIcon>
 
                 </div>
-                <div class='header-nav-two-select'>
+                <div className='header-nav-two-select'>
                     <div>Shopping Tools</div>
                     <FontAwesomeIcon icon={faChevronDown} color={'rgb(204, 0, 0)'} ></FontAwesomeIcon>
                 </div>
-                <div class='header-nav-two-select'>
+                <div className='header-nav-two-select'>
                     Find a Dealer
                 </div>
-                <div class='header-nav-two-select'>
+                <div className='header-nav-two-select'>
                     Build & Price
                 </div>
-                <div class='header-nav-two-red'>
+                <div className='header-nav-two-red'>
                     <div>
                         Local Specials
                     </div>
                 </div>
-            </div>
-{/* 
-            <div className='header-nav'>
+            </div>}
+
+            {mobNav && <div className='header-nav'>
                 <div className='header-nav-location'>
                     <img src={location} alt=''></img>
                     <p>90210</p>
@@ -86,9 +128,9 @@ function Header({openMenu, setOpenMenu}){
                 <div className='header-nav-ham'>
                     <Hamburger label="Show menu" size={26} className='header-hamburger' onToggle={()=>menuFunc()}/>
                 </div>
-            </div> */}
+            </div>}
 
-            <Collapse in={openMenu} timeout={400} className='menu'>
+            {mobNav && <Collapse in={openMenu} timeout={400} className='menu'>
                 <Carousel selectedItem={index} showArrows={false} showIndicators={false} showThumbs={false} showStatus={false}>
                     <div>
                         <MenuOne setIndex={setIndex} setMenu01={setMenu01} setMenu02={setMenu02} setMenu03={setMenu03} setMenu04={setMenu04}/>
@@ -97,7 +139,7 @@ function Header({openMenu, setOpenMenu}){
                         <LoadedMenu setIndex={setIndex} menu01={menu01} menu02={menu02} menu03={menu03} menu04={menu04} setMenu01={setMenu01} setMenu02={setMenu02} setMenu03={setMenu03} setMenu04={setMenu04}/>
                     </div>
                 </Carousel>
-            </Collapse>
+            </Collapse>}
 
 
         </div>
