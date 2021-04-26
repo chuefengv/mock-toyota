b02b6a959c0d9a1f5212ae2c8c7fbcef278af6a8
chuefengv <feng.vang98@gmail.com>
2021-04-26 01:45:00 -0400

    desktop search done

--- client/src/components/footer/deskfooter/DeskFooter.jsx
+++ client/src/components/footer/deskfooter/DeskFooter.jsx
@@ -3,22 +3,26 @@ import './DeskFooter.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import {facebook, twitter, youtube, instagram, pinterest} from '../../../assets/socials/index'
-import { text } from '@fortawesome/fontawesome-svg-core';
 
 function DeskFooter(){
 
     const [clickedSearch, setClickedSearch] = useState('false');
+
     return(
         <div className='df-wrapper'>
 
             <div className='df-searchbar'>
-                <div className-='df-searchbar-search'>
+                <div className='df-searchbar-search'>
                     <FontAwesomeIcon icon={faSearch} className='df-searchbar-search-icon'></FontAwesomeIcon>
-                    <input type='search'>
+                    <input type='search' placeholder='Type here to search'>
                     </input>
                 </div>
-                <div className-='df-searchbar-socials'>
-
+                <div className='df-searchbar-socials'>
+                    <img src={facebook} alt=''></img>
+                    <img src={youtube} alt=''></img>
+                    <img src={twitter} alt=''></img>
+                    <img src={instagram} alt=''></img>
+                    <img src={pinterest} alt=''></img>
                 </div>
             </div>
 
