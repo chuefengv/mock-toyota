9586cc6637ef6c69a2f92bf8da5bda4fc9fc3d9f
chuefengv <feng.vang98@gmail.com>
2021-05-03 22:41:52 -0400

    got sliding :after broders to work

--- client/src/components/header/cartab/CarTab.jsx
+++ client/src/components/header/cartab/CarTab.jsx
@@ -8,20 +8,20 @@ function CarTab(){
     return(
         <div className='cartab-wrapper'>
            <div className='cartab-car-types'>
-                <div className='cartab-car-types-option one'>
-                    Cars & Minivan
+                <div className={index===0 ? 'cartab-car-types-option selected one' : 'cartab-car-types-option unselected one'} onClick={()=>setIndex(0)}>
+                    <p>Cars & Minivans</p>
                 </div>
-                <div className='cartab-car-types-option two'>
-                    Trucks
+                <div className={index===1 ? 'cartab-car-types-option selected two' : 'cartab-car-types-option unselected two'} onClick={()=>setIndex(1)}>
+                    <p>Trucks</p>
                 </div>
-                <div className='cartab-car-types-option three'>
-                    Crossovers & SUVs
+                <div className={index===2 ? 'cartab-car-types-option selected three' : 'cartab-car-types-option unselected three'} onClick={()=>setIndex(2)}>
+                    <p>Crossovers & SUVs</p>
                 </div>
-                <div className='cartab-car-types-option four'>
-                    Hybrids & Fuel Cell
+                <div className={index===3 ? 'cartab-car-types-option selected four' : 'cartab-car-types-option unselected four'} onClick={()=>setIndex(3)}>
+                    <p>Hybrids & Fuel Cell</p>
                 </div>
-                <div className='cartab-car-types-option five'>
-                    Upcoming Vehicles
+                <div className={index===4 ? 'cartab-car-types-option selected five' : 'cartab-car-types-option unselected five'} onClick={()=>setIndex(4)}>
+                    <p>Upcoming Vehicles</p>
                 </div>
            </div>
         </div>
